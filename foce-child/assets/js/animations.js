dc9d2f9939d240ff60328789d11670009cc5f7af
Hidhem <hassaini.mehdi@hotmail.fr>
2026-04-10 15:18:15 +0200

    adding fallback animation(need fade out) & fix problem with other animation

--- foce-child/assets/js/animations.js
+++ foce-child/assets/js/animations.js
@@ -11,7 +11,7 @@ const observer = new IntersectionObserver(entries => {
 });
 
 
-const elements = document.querySelectorAll('#banner__title img, .characters, .place, #studio, #oscars, footer, #story__title h2, #story__article');
+const elements = document.querySelectorAll('.banner__title, .characters, .place, #studio, #oscars, footer, #story__title h2, #story__article');
 
 elements.forEach((element) => observer.observe(element));
 
@@ -32,3 +32,15 @@ const observerReverse = new IntersectionObserver(entries => {
 const element = document.querySelectorAll('.banner__header-video, #studio p, #story__article p, #place p');
 
 element.forEach((element) => observerReverse.observe(element));
+
+
+// parallax effect
+
+const bannerVideo = document.getElementById("wrapper__video");
+const bannerTitle = document.getElementById("wrapper__title");
+
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  bannerVideo.style.transform = "translateY(" + offset * 0.5 + "px)";
+  bannerTitle.style.transform = "translateY(" + offset * -0.5 + "px)";
+});
--- foce-child/front-page.php
+++ foce-child/front-page.php
@@ -4,9 +4,9 @@ get_header();
 ?>
 
     <main id="primary" class="site-main">
-        
+    
         <section id="banner" class="banner">
-
+            <!-- fallback image display -->
             <div id="banner__fallback-title" class="banner__fallback-title">
                 <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> 
                 " alt="logo Fleurs d'oranger & chats errants"/>
@@ -18,18 +18,24 @@ get_header();
             <div id="banner__fallback-image" class="banner__fallback-image">    
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/banner.png"/>
             </div>
+            <!-- banner video & title -->
 
-            <div id="banner__title" class="banner__title">
-                <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> 
-                " alt="logo Fleurs d'oranger & chats errants"/>
+            <div id="wrapper__title" class="wrapper__title">
+                <div id="banner__title" class="banner__title">
+                    <img id="banner__title-image" class="banner__title-image" src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> 
+                    " alt="logo Fleurs d'oranger & chats errants"/>
+                </div>
             </div>
 
-            <div id="banner__header-video" class="banner__header-video">
-                <video autoplay muted loop>
-                <source src="http://koukaki.local/wp-content/uploads/2026/03/StudioKoukaki-videoheadersansson1.mp4" type="video/mp4">
-            </video>
+            <div id="wrapper__video" class="wrapper__video">
+                <div id="banner__header-video" class="banner__header-video">
+                    <video autoplay muted loop>
+                    <source src="http://koukaki.local/wp-content/uploads/2026/03/StudioKoukaki-videoheadersansson1.mp4" type="video/mp4">
+                    </video>
+                </div>
             </div>
-        </section>
+
+        </section>    
         <section id="story" class="story">
             
             <article id="story__story" class="story__story">
@@ -93,8 +99,8 @@ get_header();
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
-            </section>
             <?php get_template_part( 'assets/parts/oscars' ); ?>
+        </section>
     </main><!-- #main -->
 <?php
 get_footer();
